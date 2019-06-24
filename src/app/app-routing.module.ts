2afe845c2b78f5884bb698e6d0fb6ac08b003c04
yusufrizalh <yusufrizalh@gmail.com>
2019-06-24 16:29:17 +0700

    commit this repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', loadChildren: './pages/idea-list/idea-list.module#IdeaListPageModule' },
+  { path: 'idea', loadChildren: './pages/idea-details/idea-details.module#IdeaDetailsPageModule' },
+  { path: 'idea/:id', loadChildren: './pages/idea-details/idea-details.module#IdeaDetailsPageModule' },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCrRc5qEZp7fajOhlZ5ntwXygx7BflJXdI",
+    authDomain: "event-manager-ddaf4.firebaseapp.com",
+    databaseURL: "https://event-manager-ddaf4.firebaseio.com",
+    projectId: "event-manager-ddaf4",
+    storageBucket: "event-manager-ddaf4.appspot.com",
+    messagingSenderId: "870626382102",
+    appId: "1:870626382102:web:0d6e3a59bf27560e"
+  }
 };
 
 /*
